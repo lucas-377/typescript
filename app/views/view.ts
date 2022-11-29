fd5145589d0d965c572e58fafae3312357c026dc
Lucas Santana <lucas_377@hotmail.com>
2022-11-28 21:38:32 -0300

    Abstract class and method

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,14 +1,16 @@
-export class View<T> {
+export abstract class View<T> {
   protected element: HTMLElement;
 
   constructor(selector: string) {
     this.element = document.querySelector(selector);
   }
 
-  template(model: T): string {
-    throw Error('Child class must overwrite the template method');
-  }
+  abstract template(model: T): string;
 
+  /**
+   * Update the HTML element
+   * @param model 
+   */
   update(model: T): void {
     const template = this.template(model);
     this.element.innerHTML = template;
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -2,9 +2,10 @@ export class View {
     constructor(selector) {
         this.element = document.querySelector(selector);
     }
-    template(model) {
-        throw Error('Child class must overwrite the template method');
-    }
+    /**
+     * Update the HTML element
+     * @param model
+     */
     update(model) {
         const template = this.template(model);
         this.element.innerHTML = template;
